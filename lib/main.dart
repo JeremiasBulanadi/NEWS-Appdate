8aabc2325788a6ee1669f4e580e60459d713cec4
Jeremias Bulanadi <jdb.prog@gmail.com>
2021-08-14 18:56:58 +0800

    added some updates (still unfinished)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'constants/api_path.dart';
 import 'mapPage.dart';
 import 'news.dart';
