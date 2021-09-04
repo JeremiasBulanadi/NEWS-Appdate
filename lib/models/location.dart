75c0f112c4e188251d766441de9d509eed3aa221
Jeremias Bulanadi <jdb.prog@gmail.com>
2021-09-04 20:08:44 +0800

    added extra precautions

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -8,12 +8,14 @@ class Loc {
     getDetails(loc);
   }
 
-  void getDetails(loc) async {
+  void getDetails(String loc) async {
     this.text = loc;
-    // We're not guaranteed that the entity is actually a place so this might give out null
-    this.latlng = await getLatLng(loc);
-    // Just like latlng, null is a possibility
-    this.placemark = await getPlacemark(this.latlng);
+    if (loc != '') {
+      // We're not guaranteed that the entity is actually a place so this might give out null
+      this.latlng = await getLatLng(loc);
+      // Just like latlng, null is a possibility
+      this.placemark = await getPlacemark(this.latlng);
+    }
   }
 
   late String text;
