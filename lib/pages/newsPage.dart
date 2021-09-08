da7137cf7b80daf678a5e94babef5c06a2d210cd
TristanJSV <tristanjsvasco@gmail.com>
2021-09-09 00:28:23 +0800

    reformatted the news page

--- lib/pages/newsPage.dart
+++ lib/pages/newsPage.dart
@@ -21,10 +21,16 @@ class _NewsPageState extends State<NewsPage> {
           padding: EdgeInsets.only(right: 20.0),
           child: GestureDetector(
           onTap: () {},
-          child: Icon(
+          child: Row(
+            children: [
+              Padding(padding: EdgeInsets.only(right: 15),
+              child: Icon(
               Icons.share,
               size: 26.0,
-              ),
+              ),),
+              Icon(Icons.save, size: 26.0,)
+            ],
+          )
             )
           ),
         ],
@@ -34,14 +40,43 @@ class _NewsPageState extends State<NewsPage> {
         child: Column(
           children: [
             Padding(padding: EdgeInsets.only(bottom: 25),
-            child: Text('Lorem ipsum', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),),
+            child: Align(child: Text('Lorem ipsum', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+            alignment: Alignment.centerLeft,),),
             Padding(padding: EdgeInsets.only(bottom: 25),
             child: Image.asset('lib/assets/placeholder.jpg'),),
             Padding(padding: EdgeInsets.only(bottom: 25),
             child: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')),
             Padding(padding: EdgeInsets.only(bottom: 25),
-            child: Text('#hell yeah', style: TextStyle(color: Colors.blue),),),
-            Text('Locations: Placeholder', style: TextStyle(color: Colors.black54),)
+            child: Align(child: Row(children: [
+              Text('#Placeholder1,', style: TextStyle(color: Colors.blue),),
+              Text('#Placeholder2,', style: TextStyle(color: Colors.blue),),
+              Text('#Placeholder3', style: TextStyle(color: Colors.blue),),
+               ],
+              ), alignment: Alignment.centerLeft,
+             ), 
+            ),
+            Align(child: Column(
+              children: [
+                Align(child: Text('Locations:', style: TextStyle(color: Colors.black54),), alignment: Alignment.centerLeft,),
+                Align(child: Row(children: [
+                  Text('Placeholder1', style: TextStyle(color: Colors.black54),),
+                  FlatButton(
+                    child: Icon(Icons.location_searching_outlined),
+                    onPressed: (){})
+                ],),alignment: Alignment.centerLeft,),
+                Align(child: Row(children: [
+                  Text('Placeholder2', style: TextStyle(color: Colors.black54),),
+                  FlatButton(
+                    child: Icon(Icons.location_searching_outlined),
+                    onPressed: (){})
+                ],),alignment: Alignment.centerLeft,),
+                Align(child: Row(children: [
+                  Text('Placeholder3', style: TextStyle(color: Colors.black54),),
+                  FlatButton(
+                    child: Icon(Icons.location_searching_outlined),
+                    onPressed: (){})
+                ],),alignment: Alignment.centerLeft,)
+          ],), alignment: Alignment.centerLeft,)
          ],
       ),),
     );
