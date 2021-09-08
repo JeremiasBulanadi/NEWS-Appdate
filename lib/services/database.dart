4afc8ee9e1d81f87d9147af87c09c6b9eb977faa
Jeremias Bulanadi <jdb.prog@gmail.com>
2021-09-09 01:01:58 +0800

    started with database

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,7 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
+  final String uid;
+  DatabaseService({required this.uid});
+
+  // user reference
+  final CollectionReference userCollection =
+      FirebaseFirestore.instance.collection('users');
+
   // collection reference
   final CollectionReference articleCollection =
       FirebaseFirestore.instance.collection('articles');
+
+  Future updateUserData(Map<String, int> hashtagPreferences) async {
+    return await articleCollection.doc(uid).set({
+      'hashtagPreferences': hashtagPreferences,
+    });
+  }
 }
